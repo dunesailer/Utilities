5e915e69d06d5e4f580623e9fc4b3207750ae4d6
B.T. Franklin <brandon.franklin@gmail.com>
2020-10-12 14:50:46 -0700

    Renamed String extension "parseIntoArray" to "parseIntoArrayByLine" to better clarify what the method actually does.

--- Sources/DunesailerUtilities/Extensions/String+parseIntoArray.swift
+++ Sources/DunesailerUtilities/Extensions/String+parseIntoArray.swift
@@ -1,7 +1,7 @@
 //  Created by B.T. Franklin on 7/11/18
 
 extension String {
-    public func parseIntoArray() -> [String] {
+    public func parseIntoArrayByLine() -> [String] {
         var contents = self
         contents = contents.replacingOccurrences(of: "\r", with: "\n")
         contents = contents.replacingOccurrences(of: "\n\n", with: "\n")
