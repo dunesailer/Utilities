26280349c9e8d1c1f01d3588f2a7dcabfcac4fd0
B.T. Franklin <brandon.franklin@gmail.com>
2021-01-12 00:32:49 -0700

    Added unit tests for the NSMutableAttributedString extension.

---
+++ Tests/DunesailerUtilitiesTests/Extensions/NSMutableAttributedStringUppercaseFirstCharacterTests.swift
@@ -0,0 +1,45 @@
+//  Created by B.T. Franklin on 1/12/21.
+
+import XCTest
+@testable import DunesailerUtilities
+
+final class NSMutableAttributedStringUppercaseFirstCharacterTests: XCTestCase {
+
+    func testEmptyString() {
+        let emptyString = ""
+        let mutableString = NSMutableAttributedString(string: emptyString)
+        mutableString.uppercaseFirstCharacter()
+        XCTAssertEqual(NSMutableAttributedString(string: emptyString), mutableString)
+    }
+
+    func testAlreadyUppercased() {
+        let testString = "Already uppercased"
+        let mutableString = NSMutableAttributedString(string: testString)
+        mutableString.uppercaseFirstCharacter()
+        XCTAssertEqual(NSMutableAttributedString(string: testString), mutableString)
+    }
+
+    func testNotUppercaseable() {
+        let startsWithSpace = " starts with space"
+        let mutableString1 = NSMutableAttributedString(string: startsWithSpace)
+        mutableString1.uppercaseFirstCharacter()
+        XCTAssertEqual(NSMutableAttributedString(string: startsWithSpace), mutableString1)
+
+        let startsWithUnderscore = "_starts with underscore"
+        let mutableString2 = NSMutableAttributedString(string: startsWithUnderscore)
+        mutableString2.uppercaseFirstCharacter()
+        XCTAssertEqual(NSMutableAttributedString(string: startsWithUnderscore), mutableString2)
+
+        let startsWithPercent = "%starts with percent"
+        let mutableString3 = NSMutableAttributedString(string: startsWithPercent)
+        mutableString3.uppercaseFirstCharacter()
+        XCTAssertEqual(NSMutableAttributedString(string: startsWithPercent), mutableString3)
+    }
+
+    func testLowercased() {
+        let testString = "lowercased string"
+        let mutableString = NSMutableAttributedString(string: testString)
+        mutableString.uppercaseFirstCharacter()
+        XCTAssertEqual(NSMutableAttributedString(string: "Lowercased string"), mutableString)
+    }
+}
